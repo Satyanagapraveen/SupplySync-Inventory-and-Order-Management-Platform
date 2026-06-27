64b4ef664ef38ff32e77787edb73fc6dadd1bf39
SATYA NAGA PRAVEEN KUMAR NAMBURI <153361064+Satyanagapraveen@users.noreply.github.com>
2026-06-27 15:26:56 +0530

    fixed configuration error by setting rate

--- core/throttles.py
+++ core/throttles.py
@@ -1,27 +1,32 @@
 from rest_framework.throttling import SimpleRateThrottle
 from rest_framework.exceptions import APIException
 from rest_framework import status
+from django.core.cache import cache
 
 class TooManyLoginAttemptsException(APIException):
-    status_code=status.HTTP_429_TOO_MANY_REQUESTS
-    default_code='TOO_MANY_LOGIN_ATTEMPTS'
-    default_detail='Too many failed Login attempts'
-
+    status_code = status.HTTP_429_TOO_MANY_REQUESTS
+    default_detail = 'Too many failed login attempts.'
+    default_code = 'TOO_MANY_LOGIN_ATTEMPTS'
 class LoginRateLimitThrottle(SimpleRateThrottle):
+    
+    rate = "5/m"
+    
     def get_cache_key(self, request, view):
-        ident=self.get_ident(request)
-        return f"rate-limit:login:{ident}"
         
+        ident = self.get_ident(request)
+        
+        return f"rate-limit:login:{ident}"
+    
     def allow_request(self, request, view):
+        
         self.key = self.get_cache_key(request, view)
         
         if self.key is None:
             return True
             
-        attempts = self.cache.get(self.key, 0)
+        attempts = cache.get(self.key, 0)
         
         if attempts >= 5:
             raise TooManyLoginAttemptsException()
             
         return True
-    
