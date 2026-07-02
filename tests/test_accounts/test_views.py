8e7124453dce9c2b5eba14e0c773b0d3a3b76f4b
SATYA NAGA PRAVEEN KUMAR NAMBURI <153361064+Satyanagapraveen@users.noreply.github.com>
2026-07-02 17:46:02 +0530

    Added an email field in the payload for fixing Assertion Error

--- tests/test_accounts/test_views.py
+++ tests/test_accounts/test_views.py
@@ -8,11 +8,11 @@ def test_register_returns_201_with_valid_request(api_client):
         "username": "newuser",
         "email": "newuser@test.com",
         "password": "StrongPassword123!",
-        "full_name": "New User"
+        "full_name": "New User",
+        "role": "STAFF" 
     }
     
     response = api_client.post(url, payload, format='json')
-    print("\n--- DEBUG REGISTRATION ERROR ---:", response.data)
     
     assert response.status_code == 201
     assert 'id' in response.data
@@ -24,7 +24,8 @@ def test_register_returns_409_when_email_already_exists(api_client, admin_user):
         "username": "anotheruser",
         "email": admin_user.email,
         "password": "StrongPassword123!",
-        "full_name": "Another User"
+        "full_name": "Another User",
+        "role": "STAFF"
     }
     
     response = api_client.post(url, payload, format='json')
@@ -35,10 +36,12 @@ def test_login_returns_200_with_valid_credentials(api_client, staff_user):
     url = '/api/v1/auth/login/'
     payload = {
         "username": staff_user.username,
-        "password": "TestPassword123!"
+        "password": "TestPassword123!",
+        "email" : "staff@test.com",
     }
     
     response = api_client.post(url, payload, format='json')
+    print("\n--- DEBUG Login with valid credentials ERROR ---:", response.data)
     
     assert response.status_code == 200
     assert 'access' in response.data
@@ -48,10 +51,12 @@ def test_login_returns_401_with_invalid_credentials(api_client, staff_user):
     url = '/api/v1/auth/login/'
     payload = {
         "username": staff_user.username,
-        "password": "WrongPassword!"
+        "password": "WrongPassword!",
+         "email" : "staff@test.com",
     }
     
     response = api_client.post(url, payload, format='json')
+    print("\n--- DEBUG Login ERROR ---:", response.data)
     
     assert response.status_code == 401
 
@@ -64,12 +69,13 @@ def test_refresh_token_returns_200_with_valid_refresh_token(api_client, staff_us
     login_response = api_client.post(login_url, login_payload, format='json')
     refresh_token = login_response.data.get('refresh')
     
-    refresh_url = '/api/v1/auth/refresh/'
+    refresh_url = '/api/v1/auth/token/refresh/'
     refresh_payload = {
         "refresh": refresh_token
     }
     
     response = api_client.post(refresh_url, refresh_payload, format='json')
+    print("\n--- DEBUG Token Refresh ERROR ---:", response.data)
     
     assert response.status_code == 200
     assert 'access' in response.data
